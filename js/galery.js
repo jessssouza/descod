9c37e7332faefb41efab925ff5c380216612063b
jessssouza <jessica-soares96@hotmail.com>
2024-06-28 00:24:45 -0300

    start project

---
+++ js/galery.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded",function(){
+//     const images = document.querySelectorAll(".galery-grid img")
+//     images.forEach(image =>{
+//         image.onLoad = () => {
+//             const width = image.naturalWidth * 0.05
+//             const height = image.natualHeight * 0.05
+//             image.style.width = `${width} px`
+//             image.style.height = `${height} px`
+//         }
+//     })
+// })
